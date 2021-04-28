094d982a94134ecff38b9eea0d64851c113b593a
vermelhachan790 <83366923+vermelhachan790@users.noreply.github.com>
2021-04-28 15:12:06 -0300

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,12 +1,12 @@
 #include <iostream.h>
-#include <conio.h>
+
 
 using namespace std;
 
 int main()
 {
 
-textcolor(RED);
+
 cout <<"Alerta: se você tem um celular com um processador ruim, melhor não executar o programa.\n";
 cout <<"Aperte 1, para continuar\n";
 int pass;

74733fd945051d59f8c0a3842bfc72adeb153f0c
vermelhachan790 <83366923+vermelhachan790@users.noreply.github.com>
2021-04-28 14:59:56 -0300

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -6,7 +6,18 @@ using namespace std;
 int main()
 {
 
-
+textcolor(RED);
+cout <<"Alerta: se você tem um celular com um processador ruim, melhor não executar o programa.\n";
+cout <<"Aperte 1, para continuar\n";
+int pass;
+scanf ("%i" ,pass);
+if (pass == 1)
+{
+cout <<"Agora clique 2 para abrir a trava 'Cavalo de troia'\n";
+}
+else{
+system("clear");
+}
 
 
 
